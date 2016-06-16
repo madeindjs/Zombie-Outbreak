aa9eb0c8ee9c212693a1bcab5628932c28a29dbc
madeindjs <madeindjs@gmail.com>
2016-06-16 17:14:15 +0200

    update view and begin to build IA

--- __main__.py
+++ __main__.py
@@ -10,27 +10,46 @@ from classes.zombie import Zombie
 
 if __name__ == '__main__':
 	grid = Grid()
-	grid.add_humans(1)
+	grid.add_humans(5)
+	grid.add_zombies(1)
+
+	days = 0
+
 
 	def turn():
 		# clear terminal
 		os.system('cls' if os.name == 'nt' else 'clear')
 
-		# get all areas and move all guys in theses areas
+		# get all humans/zombies and move all them
 		for human in Human.instances:
 			human.move()
-			
+
 		for zombie in Zombie.instances:
 			zombie.move()
 
+		# check all areas and find what to do
+		for area in grid.areas:
 
-		# display map and status
-		print(grid.view_map())
-		print('status: {} zombies and {} humans left'.format(Zombie.total, Human.total))
+			# they fight if there are zombies & humans in area 
+			if area.zombies !=0 and area.humans!=0:
+				pass
+
+			# a child born if there are at least two humans and no zombies
+			if area.zombies == 0 and area.humans > 1:
+				pass
+
+
 
 
 
 	while Human.total > 0 :
 		turn()
-		time.sleep(1)
+		days+=.25
+
+		# display map and status
+		print(grid.view_map())
+		print("status:\t{} zombies and {} humans left".format(Zombie.total, Human.total))
+		print("\t{} days spend".format(days))
+
+		time.sleep(.1)
 
